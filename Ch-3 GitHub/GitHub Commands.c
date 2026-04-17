4f86d3d701eed2ff5fea04610c6fe7aa10498df4
JS-Patel12 <jaypatel122010@gmail.com>
2026-04-17 22:28:20 +0530

    Ch-3

---
+++ Ch-3 GitHub/GitHub Commands.c
@@ -0,0 +1,12 @@
+1. git init
+
+2. git remote add origin https://github.com/JS-Patel12/The-C-Vault.git
+
+3. 	- git config user.email "your email"
+	- git config user.name "Username"
+
+4. git add .
+
+5. git commit -m "Message"
+
+6. git push origin master
