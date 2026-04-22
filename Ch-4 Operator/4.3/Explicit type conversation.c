02f44a0bf3c00bebafae5e6ec1cfb49996f7d7df
JS-Patel12 <jaypatel122010@gmail.com>
2026-04-22 07:30:40 +0530

    The-C-Vault

---
+++ Ch-4 Operator/4.3/Explicit type conversation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+	int num = 43;
+	float PI = 3.14;
+	
+	//int to float
+	printf("%f\n",(float)num);
+	
+	//float to int
+	printf("%d",(int)PI);
+}
---
+++ Ch-4 Operator/4.3/Implicit type conversation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+	int a = 65;
+	char Ch = 'A';
+	
+	//int to char
+	printf("%c\n",a);
+	
+	//char to int
+	printf("%d",Ch);
+}
