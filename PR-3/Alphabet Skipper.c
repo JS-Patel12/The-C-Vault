c4a61a3629444a5e6518c50d5ab7bdebd75cd15d
JS-Patel12 <jaypatel122010@gmail.com>
2026-05-15 21:18:27 +0530

    The -C-Vault

---
+++ PR-3/Alphabet Skipper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    char ch = 'a';
+    
+    start:
+    if (ch > 'z') goto end;
+    
+    if (ch < 'z')
+        printf("%c, ", ch);
+    else
+        printf("%c", ch);
+    
+    ch += 4;
+    goto start;
+    
+    end:
+    printf("\n");
+}
